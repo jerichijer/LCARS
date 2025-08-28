9b40c44dd0ec81a0996ba6113ab2bf216b297601
jer <jerichijer@proton.me>
2025-08-28 18:15:37 -0500

    initialize electron project

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 1920,
+		height: 1080
+	})
+
+	win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+	createWindow()
+
+	// opens window if none are open (macOS)
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
+})
+
+// quits app when all windows are closed (windows & linux)
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit()
+})
