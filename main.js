07e8d16bda09a2cc4ec524c3f9ee031069d03b03
jer <jerichijer@proton.me>
2025-08-28 19:24:01 -0500

    now opens in fullscreen mode

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const { app, BrowserWindow } = require('electron');
 const createWindow = () => {
 	const win = new BrowserWindow({
 		width: 1920,
-		height: 1080
+		height: 1080,
+		fullscreen: true
 	})
 
 	win.loadFile('index.html')
