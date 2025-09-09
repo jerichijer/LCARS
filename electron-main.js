d55ec7b8aea43850dfc5052a132487d4aa58eebe
jerry <jerichijer@proton.me>
2025-09-09 15:30:18 -0500

    angular setup

---
+++ electron-main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        fullscreen: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+    });
+
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/dist/temp/browser/index.html`),
+            protocol: "file:",
+            slashes: true,
+        })
+    );
+
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+    if (mainWindow === null) createWindow();
+});
