e0a115efdf26b29b4df99f45ba52a4253e1891a1
jer <jerichijer@proton.me>
2025-09-09 21:04:40 -0500

    vue + react init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<LCARSInterface />
+</template>
+
+<script setup>
+import LCARSInterface from './components/LCARSInterface.vue';
+</script>
+
+<style>
+html, body {
+	background-color: #121212;
+	color: #f5f5f5;
+	font-family: Arial, sans-serif;
+	overflow: hidden;
+}
+</style>
---
+++ src/components/LCARSInterface.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="container">
+		<div class="txt">
+			<h1>LCARS Interface</h1>
+			<p>Vue + Electron</p>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+html, body {
+	background-color: #121212;
+	color: #f5f5f5;
+}
+
+.container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+	font-family: Arial, sans-serif;
+}
+
+.txt {
+	text-align: center;
+}
+</style>
